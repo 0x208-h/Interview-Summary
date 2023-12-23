e3d92051790542d82e5c6ccebf0131971728380c
0x208-h <huangchenhui0812@163.com>
2023-12-23 21:53:39 +0800

    feat: 堆化

--- 算法/MinHeap/index.js
+++ 算法/MinHeap/index.js
@@ -87,9 +87,8 @@ h.size();
 console.log(h.peek()); // 2
 console.log(h.size()); // 3
 
-
 // const heap = heapify([...nums]);
-    
+
 // let res = 0;
 // while (k--) {
 //     res += heap[0];
@@ -97,7 +96,6 @@ console.log(h.size()); // 3
 //     sink(heap, 0);
 // }
 // return res;
-
 
 // function heapify(origin: number[]) {
 //     for (let i = Math.floor(origin.length / 2) - 1; i >= 0; i--) {
@@ -119,3 +117,81 @@ console.log(h.size()); // 3
 //         i = index;
 //     }
 // }
+let heapSize = nums.length;
+buildMaxHeap(nums, heapSize); // 构建好了一个大顶堆
+// 进行下沉 大顶堆是最大元素下沉到末尾
+for (let i = nums.length - 1; i >= nums.length - k + 1; i--) {
+  swap(nums, 0, i);
+  --heapSize; // 下沉后的元素不参与到大顶堆的调整
+  console.log(nums);
+  // 重新调整大顶堆
+  maxHeapify(nums, 0, heapSize);
+}
+return nums[0];
+// 自下而上构建一颗大顶堆
+function buildMaxHeap(nums, heapSize) {
+  for (let i = Math.floor(heapSize / 2) - 1; i >= 0; i--) {
+    maxHeapify(nums, i);
+  }
+}
+// 从左向右，自上而下的调整节点
+function maxHeapify(nums, i) {
+  let l = i * 2 + 1;
+  let r = i * 2 + 2;
+  let largest = i;
+  if (l < heapSize && nums[l] > nums[largest]) {
+    largest = l;
+  }
+  if (r < heapSize && nums[r] > nums[largest]) {
+    largest = r;
+  }
+  if (largest !== i) {
+    swap(nums, i, largest); // 进行节点调整
+    // 继续调整下面的非叶子节点
+    maxHeapify(nums, largest);
+  }
+}
+function swap(a, i, j) {
+  let temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+
+
+var minStoneSum = function (piles, k) {
+  heapify(piles); // 堆化
+  while (k-- && piles[0] !== 1) {
+    piles[0] -= Math.floor(piles[0] / 2); // 直接修改堆顶
+    sink(piles, 0); // 堆化（只需要把 piles[0] 下沉）
+  }
+  return _.sum(piles);
+};
+
+// 原地堆化（最大堆）
+// 堆化可以保证 h[0] 是堆顶元素，且 h[i] >= max(h[2*i+1], h[2*i+2])
+function heapify(h) {
+  // 倒着遍历，从而保证 i 的左右子树一定是堆，那么 sink(h, i) 就可以把左右子树合并成一个堆
+  // 下标 >= h.length / 2 的元素是二叉树的叶子，无需下沉
+  for (let i = Math.floor(h.length / 2) - 1; i >= 0; i--) {
+    sink(h, i);
+  }
+}
+
+// 把 h[i] 不断下沉，直到 i 的左右儿子都 <= h[i]
+function sink(h, i) {
+  const n = h.length;
+  while (2 * i + 1 < n) {
+    let j = 2 * i + 1; // i 的左儿子
+    if (j + 1 < n && h[j + 1] > h[j]) {
+      // i 的右儿子比 i 的左儿子大
+      j++;
+    }
+    if (h[j] <= h[i]) {
+      // 说明 i 的左右儿子都 <= h[i]，停止下沉
+      break;
+    }
+    [h[i], h[j]] = [h[j], h[i]]; // 下沉
+    i = j;
+  }
+}
